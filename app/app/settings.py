42f4ab3ccb7e7d54aefce43245dcac2e6a2410b6
ShaniEnginer <engusman8279@gmail.com>
2020-07-06 13:03:13 +0500

    clean up settings.py file

--- app/app/settings.py
+++ app/app/settings.py
@@ -87,17 +87,6 @@ DATABASES = {
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'mydatabase',
-#         'USER': 'mydatabaseuser',
-#         'PASSWORD': 'mypassword',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
